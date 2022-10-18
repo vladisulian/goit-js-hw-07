067a6663841a7d2025a056f592568d76cbc9a984
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-10-19 01:13:08 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -17,7 +17,7 @@ function createGalleryItems(galleryItems) {
     .map(({ preview, original, description }) => {
       return `
       <div class="gallery__item">
-      <a class="gallery__link" href="large-image.jpg">
+      <a class="gallery__link" href=${preview}>
         <img
           class="gallery__image"
           src="${original}"
