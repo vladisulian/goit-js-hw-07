ade12e8a0bb1c593cee2e2579e5b8f8c0efc32c1
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-10-19 23:39:51 +0300

    done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryList = document.querySelector(".gallery");
+const galleryIMGItems = createGalleryItems(galleryItems);
+galleryList.insertAdjacentHTML("beforeend", galleryIMGItems);
+// console.log(galleryItems);
 
-console.log(galleryItems);
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}" >
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            title="${description}"
+          />
+        </a>
+        `;
+    })
+    .join("");
+}
+
+const gallery = new SimpleLightbox(".gallery a");
+gallery.on("show.simplelightbox", function () {
+  captionDelay: "250";
+});
