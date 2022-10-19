f223de1266d1335ed36061e079d3895e5b88eaaa
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-10-19 20:43:01 +0300

    Add these min.css and .js files

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,29 +4,38 @@ import { galleryItems } from "./gallery-items.js";
 const galleryList = document.querySelector(".gallery");
 const galleryIMGItems = createGalleryItems(galleryItems);
 galleryList.insertAdjacentHTML("beforeend", galleryIMGItems);
+galleryList.addEventListener("click", onGalleryItemsClick);
 
 function createGalleryItems(galleryItems) {
-  //   galleryItems.forEach((element) => {
-  //     return `
-  //       <div class='gallery-item'>
-  //           <img src='${elementoriginal}', alt ='gallery-item'>
-  //       </div>
-  //       `;
-  //   });
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
-      <div class="gallery__item">
-      <a class="gallery__link" href=${original}>
-        <img
-          class="gallery__image"
-          src="${original}"
-          data-source="${preview}"
-          alt="${description}"
-        />
-      </a>
-    </div>
-        `;
+      <div class ="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" 
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"/>
+        </a>
+      </div>`;
     })
     .join("");
+}
+function onGalleryItemsClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`<img src=${e.target.dataset.source}>`);
+  instance.show();
+  document.addEventListener("keydown", onModalCloseToEscape);
+  function onModalCloseToEscape(e) {
+    if (e.code === "Escape") {
+      instance.close();
+      document.removeEventListener("keydown", onModalCloseToEscape);
+    }
+  }
+
+  console.log("That's picture");
 }
